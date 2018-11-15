c0d1d440b4caa215aab09152ef727bf6d7f78195
Dennygod <dennygod1220@gmail.com>
2018-11-15 09:04:08 +0800

    1115

--- public/JS/DemoPage_Zone_set/nownews_m_300250.js
+++ public/JS/DemoPage_Zone_set/nownews_m_300250.js
@@ -14,19 +14,22 @@ function SetCusZone(html,path,zone) {
 
 function setDefaultZone(html, path) {
   //讓中文圖片url不會變亂碼
-  var img_dir = fs.readdirSync(path + '/images/');
-  img_dir.forEach(element => {
-    fs.renameSync(path + '/images/' + element, path+ '/images/' + decodeURIComponent(escape(element)));
-  });
+  // var img_dir = fs.readdirSync(path + '/images/');
+  // img_dir.forEach(element => {    
+  //   fs.renameSync(path + '/images/' + element, path+ '/images/' + decodeURIComponent(escape(element)));
+  // });
 
   var $ = cheerio.load(html);
   $(".etad").children().remove();
-  $(".etad").append('<div style="text-align: center;"><ins class="clickforceads" style="display:inline-block;width:970px;height:250px;" data-ad-zone="8727"></ins><script async type="text/javascript" src="//cdn.doublemax.net/js/init.js"></script></div>');
+  $(".etad").append('<div style="text-align: center;"><ins class="clickforceads" style="display:inline-block;width:300px;height:250px;" data-ad-zone="8707"></ins><script async type="text/javascript" src="//cdn.doublemax.net/js/init.js"></script></div>');
   fs.writeFile(path+'/DefaultZone.html', $.html(), function () {
     console.log('旅食樂 300250 手機板 預設版位 OK')
   });
 
 }
+
+
+
 
 module.exports = {
   setDefaultZone: setDefaultZone,
